12b279b123892e5b737af56cc51403a8c563f8d5
Emanuel Zini-Casaro <emanuel.zini-casaro@appdirect.com>
2025-03-07 18:29:26 -0300

    updated testdata file

--- fixtures/data/test-data.ts
+++ fixtures/data/test-data.ts
@@ -2,12 +2,6 @@
 
 // Interface for Test Data
 export interface TestData {
-  USER: {
-    EMAIL: string;
-    PASSWORD: string;
-    INVALID_EMAIL: string;
-    INVALID_PASSWORD: string;
-  };
   PRODUCT: {
     NAME: string;
     IMAGE_PATH: string;
@@ -17,12 +11,6 @@ export interface TestData {
 }
 
 export const TEST_DATA: TestData = {
-  USER: {
-    EMAIL: process.env.TEST_USER_EMAIL || 'testuser@example.com',
-    PASSWORD: process.env.TEST_USER_PASSWORD || 'testpassword123',
-    INVALID_EMAIL: 'invalid@example.com',
-    INVALID_PASSWORD: 'wrongpassword'
-  },
   PRODUCT: {
     NAME: 'The Big Piza',
     IMAGE_PATH: 'fixtures/images/wp1813257.jpg',
